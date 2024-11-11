1ad2c474a895638970521a4bec28559f0c2eedb1
Andrei. <zrojeat@gmail.com>
2024-11-11 21:31:25 +0800

    Update supabase.dart

--- lib/supabase/supabase.dart
+++ lib/supabase/supabase.dart
@@ -5,7 +5,7 @@ final supabase = Supabase.instance.client;
 
 Future<void> initSupabase() async {
   await Supabase.initialize(
-    url: 'https://ugvcmfripsepimuwetav.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVndmNtZnJpcHNlcGltdXdldGF2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYyODM4MTAsImV4cCI6MjA0MTg1OTgxMH0.jdOtxDcv6ZkDlNCQrzEhxaoyWaji4ThzBE65AG0bpqo',
+    url: '',
+    anonKey: '',
   );
 }
